54d6b96641eedc088fde06012cc7eca5ed68f956
Felipe Cantini <114841536+fificici@users.noreply.github.com>
2024-12-12 20:05:44 -0300

    Add files via upload

---
+++ RPG/RPG/app/src/main/java/RPG/Pokemon.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package RPG;
+
+/**
+ *
+ * @author FELIPECANTINI
+ */
+public interface Pokemon {
+    
+    void atacar(Oponente oponente);
+    void atacarForte(Oponente oponente);
+    boolean estaVivo();
+    
+}
